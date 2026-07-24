a3334e30ea3b83a1060bdcec8d6594c6818dca33
jayoadonis <jayo.adonisraphael@gmail.com>
2026-07-24 20:16:46 +0800

    [build] Add shared library config and corresponding export header

--- cmake-3-23-way/src/main/cpp/public/learn_cpp/project_structure/cmake_3_23_way/test_x.h
+++ cmake-3-23-way/src/main/cpp/public/learn_cpp/project_structure/cmake_3_23_way/test_x.h
@@ -1,9 +1,11 @@
 #ifndef LEARN_CPP_PROJECT_STRUCTURE_CMAKE_3_23_WAY_TEST_H
 #define LEARN_CPP_PROJECT_STRUCTURE_CMAKE_3_23_WAY_TEST_H
 
+#include "cmake_3_23_way_export.h"
+
 namespace learn_cpp::project_structure::cmake_3_23_way {
 
-  void test_x(float x);
+  CMAKE_3_23_WAY_API void test_x(float x);
 }
 
 #endif
